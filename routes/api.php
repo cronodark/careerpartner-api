81e443cea8db7aba96b28787e3fe56a96d155f09
cronodark <kevinfarhan95@gmail.com>
2025-07-05 15:12:27 +0700

    kevin, features/talent-data-cud

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,17 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\talent\TalentController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::middleware('auth:sanctum')->group(function () {
+    Route::middleware('role:talent')->group(function () {
+        Route::get('/talent', [TalentController::class, 'index'])->name('talent.index');
+        Route::put('/talent', [TalentController::class, 'update'])->name('talent.update');
+        Route::delete('/talent', [TalentController::class, 'destroy'])->name('talent.destroy'); 
+
+    });
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
